7bb53cff88c4ac04eac3ff68b3edf6dd91ba3719
Oleg <schika145@gmail.com>
2023-10-24 13:44:39 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив на 10 целых чисел. Написать программу которая определяет количество чётных чисел в массиве
+
+int namber = 0;
+int[] array = new int[10];
+for (int i = 0; i < 10; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.Write(array[i] + " ");
+    int a = array[i] % 2;
+    if (a == 0)
+    {
+        namber = namber + 1;
+    }
+}
+Console.WriteLine(" - " + namber);
